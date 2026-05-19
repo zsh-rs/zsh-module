18085f6ed2c0f71e30465fbc07ff0efd0c9539ec
Cole McAnelly <cole.mcanelly@valentpartners.com>
2026-05-19 17:05:32 -0500

    fix: add features' pointers to object, comment out paramdef until pattern is stable

--- src/private/api/globals.rs
+++ src/private/api/globals.rs
@@ -12,8 +12,8 @@ pub static CONDITIONS: [zsh::conddef]; // todo!("Conditions are not yet implemen
 #[linkme::distributed_slice]
 pub static MATH_FUNCS: [zsh::mathfunc]; // todo!("Math functions are not yet implemented");
 
-#[linkme::distributed_slice]
-pub static PARAMETERS: [zsh::paramdef]; // todo!("Parameter hooks are not yet implemented");
+// #[linkme::distributed_slice]
+// pub static PARAMETERS: [zsh::paramdef]; // todo!("Parameter hooks are not yet implemented");
 
 
 /// To be used if it is found that ZSH is causing segfaults by writing to the non-mutable static
@@ -22,10 +22,10 @@ pub static PARAMETERS: [zsh::paramdef]; // todo!("Parameter hooks are not yet im
 pub static mut FEATURES: LazyCell<zsh::features> = LazyCell::new(|| zsh::features {
     bn_list: BUILTINS.as_ptr() as _,
     bn_size: BUILTINS.len() as _,
-    cd_list: std::ptr::null_mut(),
-    cd_size: 0,
-    mf_list: std::ptr::null_mut(),
-    mf_size: 0,
+    cd_list: CONDITIONS.as_ptr() as _,
+    cd_size: CONDITIONS.len() as _,
+    mf_list: MATH_FUNCS.as_ptr() as _,
+    mf_size: MATH_FUNCS.len() as _,
     pd_list: std::ptr::null_mut(),
     pd_size: 0,
     n_abstract: 0,
